09af1253d9660134ef5156332147cf5775c605c5
Luis Coco Enríquez <LEnriquez@luxoft.com>
2023-08-08 12:10:07 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+// Client code
+class Program
+{
+    static void Main(string[] args)
+    {
+        var shapes = new IShape[] { new Circle(), new Square() };
+        var areaVisitor = new AreaVisitor();
+
+        foreach (var shape in shapes)
+        {
+            shape.Accept(areaVisitor);
+        }
+    }
+}
+
+// Shape hierarchy
+interface IShape
+{
+    void Accept(IVisitor visitor);
+}
+
+class Circle : IShape
+{
+    public void Accept(IVisitor visitor)
+    {
+        visitor.VisitCircle(this);
+    }
+}
+
+class Square : IShape
+{
+    public void Accept(IVisitor visitor)
+    {
+        visitor.VisitSquare(this);
+    }
+}
+
+// Visitor interface
+interface IVisitor
+{
+    void VisitCircle(Circle circle);
+    void VisitSquare(Square square);
+}
+
+// Concrete visitor implementation
+class AreaVisitor : IVisitor
+{
+    public void VisitCircle(Circle circle)
+    {
+        Console.WriteLine("Calculating area of Circle");
+    }
+
+    public void VisitSquare(Square square)
+    {
+        Console.WriteLine("Calculating area of Square");
+    }
+}
+
